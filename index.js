cb5e4dc890f1c0bffeddd53ac797fdddd5b34ce8
Martin Pereyra <pereyra.gamboa@outlook.com>
2018-10-25 21:00:39 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const os = require('os');
+const express = require('express');
+
+console.clear();
+console.log(os.platform());
+console.log(os.userInfo());
+console.log(os.cpus());
+
+const app = express();
+
+app.get('/', (req, res) => res.send('FNORD!'));
+app.get('/about', (req, res) => res.send('v0.1'));
+app.get('/contact', (req, res) => res.send('Inquiries: pereyra@example.com'));
+
+app.listen(3000);
