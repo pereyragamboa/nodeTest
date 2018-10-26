ae01d0bddf75801d0ed2b766b740346eec55ec97
Martin Pereyra <pereyra.gamboa@outlook.com>
2018-10-25 21:57:49 -0500

    Changes for Heroku deployment

--- index.js
+++ index.js
@@ -12,4 +12,5 @@ app.get('/', (req, res) => res.send('foo bar'));
 app.get('/about', (req, res) => res.send('v0.1'));
 app.get('/contact', (req, res) => res.send('Inquiries: pereyra@example.com'));
 
-app.listen(3000);
+// process.env.PORT required for Heroku deployment
+app.listen(process.env.PORT || 3000);
